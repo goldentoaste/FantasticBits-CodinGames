63df39a23d025186dcd69c82d31a669994793172
goldentoaste <rayg2375@gmail.com>
2023-08-03 22:03:09 -0700

    wip

--- main.py
+++ main.py
@@ -1,20 +1,58 @@
 import sys, math
-
+from typing import List, Dict, Iterable
 
 #### Consts ####
 
 LEFT = 0  # need to score right
 RIGHT = 1  # own goal is on right side, need to score left
 SIDE = LEFT  # to be init later in main
+
+
+# field/coords can be at most 16000 x 7500
+W = 16000
+H = 7500
+
+
 #### End Consts ####
+
+### --- ###
+
+#### Game Vars ####
+
+
+allies: Dict[int, "Wizard"] = {}
+opponents: Dict[int, "Wizard"] = {}
+sniffles: Dict[int, "Sniffle"] = {}
+
+gameTime = 0  # up to 200, in turns
+
+allieScore = 0
+allieMagic = 0
+opponentScore = 0
+opponentMagic = 0
+
+allieGoal = (0, 0)  # 2 point representing allie goal
+opponentGoal = (0, 0)  # point for opponent goal
+
+#### End Game Vars####
+
+
+def firstNot(target, iterable: Iterable):
+    for item in iterable:
+        if item != target:
+            return item
+    return None
 
 
 class V2:
     # Vector 2 class
-
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
+
+    @classmethod
+    def ZERO(cls):
+        return V2(0,0)
 
     def __str__(self):
         return f"V2({self.x}, {self.y})"
@@ -24,6 +62,9 @@ class V2:
 
     def __mul__(self, target: int):
         return V2(self.x * target, self.y * target)
+
+    def __rmul__(self, target: int):
+        return self.__mul__(target)
 
     def __sub__(self, target: "V2") -> "V2":
         return self + (-1 * target)
@@ -45,13 +86,38 @@ class V2:
         """Calc for the smaller vector between the 2 vectors, in radians"""
         return math.acos(self.normalized().dot(target.normalized()))
 
+    def distTo(self, target: "V2"):
+        return (target - self).mag()
+
+    @classmethod
+    def avg(cls, vectors: List["V2"]) -> "V2":
+        return sum(vectors, start=V2.ZERO()) / len(vectors)
+
+
+#### init ####
+SIDE = int(input())  # which side are we playing on
+
+allieGoal = (V2(0, 1750), V2(0, 5750))  # assume we are left side
+opponentGoal = (V2(16000, 1750), V2(16000, 5750))
+
+# swap if right side
+if SIDE:
+    allieGoal, opponentGoal = opponentGoal, allieGoal
+
+#### end init ####
+
 
 class Entity:
     def __init__(self, eid: str, pos: V2, vel: V2):
         self.id = eid
         self.pos = pos
         self.vel = vel
-        
+        self.type = "undefined"
+
+    def update(self, pos: V2, vel: V2):
+        self.pos = pos
+        self.vel = vel
+
     @property
     def x(self):
         return self.pos.x
@@ -68,38 +134,142 @@ class Entity:
     def vy(self):
         return self.vel.y
 
-
     def __str__(self):
         return f"Entity({self.id}), pos:{self.pos}, vel: {self.vel}"
-    
-    def currentHeading(self)->V2:
-        '''
-        returns the position this object will end up at assuming no velocity changes
-        '''
-        
-        return self.pos + self.vel
-    
-    def calcInterceptCourse(self, target: 'Entity', maxThrust : int):
 
-        '''
+    def currentHeading(self) -> V2:
+        """
+        returns the position this object will end up at assuming no velocity changes
+        """
+
+        return self.pos + self.vel
+
+    def calcInterceptCourse(self, target: "Entity", maxThrust: int):
+        """
         target: other entity this one will try to intercept.
         maxThrust: the *max* magnitude this obj can accelerate by in 1 turn.
-        
+
         returns the expected point of interception and max thrust needed to intercept.
-        Use max thrust if interception not possible in 1 frame, 
+        Use max thrust if interception not possible in 1 frame,
         but, if possible, calc the exact needed velocity change
-        '''
-        
-        
-        
-        
-        return target.currentHeading(), maxThrust
-        
-        
-        
-        
-        
-    
+        """
+
+        # err
+        targetPos = target.currentHeading()
+
+        # error is much we need to correct by
+        error = targetPos - self.pos - self.vel
+
+        # solve for thrust needed in the direction of targetPos
+        # thrust cannot exceed 'maxThrust'
+        thrust = min(maxThrust, error.mag())
+
+        return target.currentHeading(), thrust
+
+
+class Sniffle(Entity):
+    def __init__(self, eid: str, pos: V2, vel: V2, grabbed=False):
+        super().__init__(eid, pos, vel)
+        self.type = "sniffle"
+        self.grabbed = grabbed
+        self.targetted = False
+
+    def update(self, pos: V2, vel: V2, grabbed=False):
+        super().update(pos, vel)
+        self.grabbed = bool(grabbed)
+
+
+class Wizard(Entity):
+    def __init__(self, eid: int, pos: V2, vel: V2, grabbed=False):
+        super().__init__(eid, pos, vel)
+        self.type = "wizard"
+        self.grabbed = grabbed
+
+    def update(self, pos: V2, vel: V2, grabbed: bool = False):
+        super().update(pos, vel)
+        self.grabbed = bool(grabbed)
+
+    def play(
+        self,
+        allie: "Wizard",
+        sniffles: Dict[int, Sniffle],
+        opponents: Dict[int, "Wizard"],
+    ):
+        # logic for shooting
+        if self.grabbed:
+            goal = V2.avg(opponentGoal)
+            print(f"THROW {goal.x} {goal.y} 500")
+            return
+
+        minSniffle = sniffles[list(sniffles.keys())[0]]
+        d = 999999
+        for s in sniffles.values():
+            # TODO dont give up balls later
+            if s.grabbed or s.targetted:
+                continue
+
+            dist = self.currentHeading().distTo(s.currentHeading())
+            if dist < d:
+                minSniffle = s
+            else:
+                if s.targetted == self.id:
+                    s.targetted = None
+
+        if minSniffle:
+            minSniffle.targetted = self.id
+
+            direction, thrust = self.calcInterceptCourse(minSniffle, 150)
+            print(f"MOVE {direction.x} {direction.y} {thrust}")
+            return
+
+        # default behaviour
+
+        print("MOVE 8000 3750 150")
+
+
+def updateCycle():
+    global allieScore, allieMagic, opponentScore, opponentMagic
+
+    # just following thier template
+    allieScore, allieMagic = [int(i) for i in input().split()]
+    opponentScore, opponentMagic = [int(i) for i in input().split()]
+
+    entityCount = int(input())  # number of ents
+
+    for i in range(entityCount):
+        # 1 entity per line
+        vals = input().split()
+
+        eid = int(vals[0])
+        etype = vals[1]
+        pos = V2(int(vals[2]), int(vals[3]))
+        vel = V2(int(vals[4]), int(vals[5]))
+        grabbed = bool(int(vals[6]))
+
+        if etype == "WIZARD":
+            try:
+                allies[eid].update(pos, vel, grabbed)
+            except KeyError:
+                obj = Wizard(eid, pos, vel, grabbed)
+                allies[eid] = obj
+
+        elif etype == "OPPONENT_WIZARD":
+            try:
+                opponents[eid].update(pos, vel, grabbed)
+            except KeyError:
+                obj = Wizard(eid, pos, vel, grabbed)
+                opponents[eid] = obj
+
+        elif etype == "SNAFFLE":
+            try:
+                sniffles[eid].update(pos, vel, grabbed)
+            except KeyError:
+                obj = Sniffle(eid, pos, vel, grabbed)
+                sniffles[eid] = obj
+
+
 if __name__ == "__main__":
-    SIDE = int(input())  # should be either left or right
-    pass
+    while True:
+        updateCycle()
+        for w in allies.values():
+            w.play(allies[firstNot(w.id, allies)], sniffles, opponents)
