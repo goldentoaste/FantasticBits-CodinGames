e4258589af0b309144c0f57089ce98276ffc21cb
goldentoaste <rayg2375@gmail.com>
2023-08-03 18:32:54 -0700

    boiler plate

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FantasticBits-CodinGames
- 2d harry potter game
+ 2d harry potter game  
+ Attempts for python solution to [this game](https://www.codingame.com/ide/puzzle/fantastic-bits)  
+ As there seems be no good way to merging python scripts, and codingames expects a single file, the entire solution will be under main.py
+ 
---
+++ main.py
@@ -0,0 +1,94 @@
+import sys, math
+
+
+#### Consts ####
+
+LEFT = 0  # need to score right
+RIGHT = 1  # own goal is on right side, need to score left
+SIDE = LEFT  # to be init later in main
+#### End Consts ####
+
+
+class V2:
+    # Vector 2 class
+
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"V2({self.x}, {self.y})"
+
+    def __add__(self, target: "V2"):
+        return V2(self.x + target.x, self.y + target.y)
+
+    def __mul__(self, target: int):
+        return V2(self.x * target, self.y * target)
+
+    def __sub__(self, target: "V2") -> "V2":
+        return self + (-1 * target)
+
+    def __div__(self, target: int) -> "V2":
+        return self * (1 / target)
+
+    def dot(self, target: "V2"):
+        """Dot product of this vector and another"""
+        return self.x * target.x + self.y + target.y
+
+    def mag(self):
+        return math.sqrt(self.x * self.x + self.y * self.y)
+
+    def normalized(self) -> "V2":
+        return self / self.mag()
+
+    def angleTo(self, target: "V2") -> float:
+        """Calc for the smaller vector between the 2 vectors, in radians"""
+        return math.acos(self.normalized().dot(target.normalized()))
+
+
+class Entity:
+    def __init__(self, name: str, pos: V2, vel: V2):
+        self.name = name
+        self.pos = pos
+        self.vel = vel
+
+    @property
+    def x(self):
+        return self.pos.x
+
+    @property
+    def y(self):
+        return self.pos.y
+
+    @property
+    def vx(self):
+        return self.vel.x
+
+    @property
+    def vy(self):
+        return self.vel.y
+
+
+    def __str__(self):
+        return f"Entity({self.name}), pos:{self.pos}, vel: {self.vel}"
+    
+    def calcInterceptCourse(self, target: 'Entity', maxThrust : int):
+
+        '''
+        target: other entity this one will try to intercept.
+        maxThrust: the *max* magnitude this obj can accelerate by in 1 turn.
+        
+        returns the expected point of interception and max thrust needed to intercept.
+        at the moment always use the max thrust.
+        '''
+        
+        
+        
+        
+        
+        
+        pass
+    
+if __name__ == "__main__":
+    SIDE = int(input())  # should be either left or right
+    pass
